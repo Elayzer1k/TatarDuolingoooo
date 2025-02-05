8ad955d60846c6373268d5797ce1fb56d4fcd020
Elayzer1k <salakovamir@gmail.com>
2025-02-05 17:33:19 +0300

    version 0.2

--- script.py
+++ script.py
@@ -2,9 +2,9 @@ from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
 
 
-class MainApp(MDApp):
+class TatarDuolingoooo(MDApp):
     def build(self):
         return MDLabel(text="Hello, World", halign="center")
 
 
-MainApp().run()
+TatarDuolingoooo().run()
